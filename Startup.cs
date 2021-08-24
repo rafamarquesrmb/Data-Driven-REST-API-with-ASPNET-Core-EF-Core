6b6cc8f8f5bfd6159f90cd25bc604484a4035860
rafamarquesrmb <rafa.rmb@gmail.com>
2021-08-23 22:46:45 -0300

    Adding cors

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddResponseCompression(options => 
             {
                 options.Providers.Add<GzipCompressionProvider>();
@@ -67,6 +68,8 @@ namespace Shop
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthentication();
 
